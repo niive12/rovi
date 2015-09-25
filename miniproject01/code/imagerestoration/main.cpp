be1c3bda3d50e43a6443558a3d5cf11d2de8515e
niive12 <niive12@student.sdu.dk>
2015-09-25 17:17:51 +0200

    I am a bad programmer

--- miniproject01/code/imagerestoration/main.cpp
+++ miniproject01/code/imagerestoration/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>)
 
 #include "histogram.h"
 #include "median_filter.h"
 #include <vector>
 
+#define WIDTH 1000
+#define HEIGHT 500
+
 int main(){
     std::cout << "Hello World!" << "\n";
 
-    cv::Mat image = cv::imread( "../images/Image2.png", CV_LOAD_IMAGE_GRAYSCALE );
-
-    if(!image.data){
-        std::cout << "img read\n";
-
-    }
     std::vector<std::string> image_names;
     image_names.push_back("../images/Image1.png"         );
     image_names.push_back("../images/Image2.png"         );
@@ -24,20 +22,27 @@ int main(){
     image_names.push_back("../images/Image5_optional.png");
 
 
+    cv::Mat image = cv::imread( image_names.at(1) , CV_LOAD_IMAGE_GRAYSCALE );
+    cv::Mat modified = image.clone();
+    namedWindow("Restored Image", cv::WINDOW_NORMAL);
+//    median_filter(image,modified,7,1);
+    cv::medianBlur(image,modified,7);
+    cv::imshow( "Restored Image", modified);
+    cv::resizeWindow("Restored Image", WIDTH, HEIGHT);
+    cv::Mat histogram;
+    cv::Mat histImage( 512, 1024, CV_8UC3 );
+    make_histogram(image,histImage,histogram,1024);
+//    cv::imshow( "Original Histogram", histImage);
+    cv::waitKey(0);
+    /*
     for(auto i : image_names){
         cv::Mat image = cv::imread( i , CV_LOAD_IMAGE_GRAYSCALE );
         cv::Mat histogram;
         cv::Mat histImage( 512, 1024, CV_8UC3 );
-        cv::Mat modified = image.clone();
-        median_filter(image,modified,7,1);
-        //    cv::imshow( "Original Image", image);
-            cv::imshow( "Restored Image", modified);
         make_histogram(image,histImage,histogram,1024);
         cv::imshow( "Original Histogram", histImage);
-
-//        cv::waitKey(0);
-    }
-
+        cv::waitKey(0);
+    }//*/
     return 0;
 }
 
