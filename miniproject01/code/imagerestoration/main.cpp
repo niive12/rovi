cd470b5340fa7f88522ae4ce3be0030b6c96fcb7
Croomch <lschw12@student.sdu.dk>
2015-09-25 16:26:50 +0200

    code

--- miniproject01/code/imagerestoration/main.cpp
+++ miniproject01/code/imagerestoration/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
 #include "histogram.h"
 
-int main(int argc, char** argv ){
+int main(){
     std::cout << "Hello World!" << "\n";
-    if ( argc != 2 ) {
-        //No single image passed as argument
-        return -1;
+
+    cv::Mat image = cv::imread( "../images/Image2.png", CV_LOAD_IMAGE_GRAYSCALE );
+
+    if(!image.data){
+        std::cout << "img read\n";
+
     }
 
-    cv::Mat image = cv::imread( "../images/Image1.png", CV_LOAD_IMAGE_GRAYSCALE );
+    std::cout << "img read\n";
     cv::Mat histogram;
     cv::Mat histImage( 512, 1024, CV_8UC3 );
     make_histogram(image,histImage,histogram,1024);
 //    cv::imshow( "Original Image", image);
     cv::imshow( "Original Histogram", histImage);
+
+
 
     cv::waitKey(0);
 
