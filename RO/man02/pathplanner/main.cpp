cb4ec4e380d6f5f235c6a39e46466e3637ab2671
Croomch <lschw12@student.sdu.dk>
2015-10-07 11:44:13 +0200

    Now with path planning

--- RO/man02/pathplanner/main.cpp
+++ RO/man02/pathplanner/main.cpp
@@ -9,30 +9,36 @@
 
 #include <string>
 
-using namespace std;
+#define MAXTIME 10.
 
 
-void outputLuaPath(rw::trajectory::QPath &path, string &robot){
+// Kinematics::gripFrame()
+// new state
+// remember to update planner with new state
+
+
+
+void outputLuaPath(rw::trajectory::QPath &path, std::string &robot){
     // output default stuff
-    cout << "wc = rws.getRobWorkStudio():getWorkCell()\n"
-            "state = wc:getDefaultState()\n"
-            "device = wc:findDevice(\""
-            << robot << "\")\n\n"
-            "function setQ(q)\n"
-            "qq = rw.Q(#q,q[1],q[2],q[3],q[4],q[5],q[6])\n"
-            "device:setQ(qq,state)\n"
-            "rws.getRobWorkStudio():setState(state)\n"
-            "rw.sleep(1)\n"
-            "end\n\n";
+    std::cout << "wc = rws.getRobWorkStudio():getWorkCell()\n"
+                 "state = wc:getDefaultState()\n"
+                 "device = wc:findDevice(\""
+              << robot << "\")\n\n"
+                 "function setQ(q)\n"
+                 "qq = rw.Q(#q,q[1],q[2],q[3],q[4],q[5],q[6])\n"
+                 "device:setQ(qq,state)\n"
+                 "rws.getRobWorkStudio():setState(state)\n"
+                 "rw.sleep(1)\n"
+                 "end\n\n";
 
     for (rw::trajectory::QPath::iterator it = path.begin(); it < path.end(); it++) {
-        cout << "setQ({"
-                << (*it)[0];
+        std::cout << "setQ({"
+                  << (*it)[0];
 
         for(int i = 1; i < (*it).size(); i++){
-            cout << "," << (*it)[i];
+            std::cout << "," << (*it)[i];
         }
-        cout << "})\n";
+        std::cout << "})\n";
     }
 }
 
@@ -40,16 +46,46 @@ void outputLuaPath(rw::trajectory::QPath &path, string &robot){
 int main()
 {
     // name of the device in the WC
-    string deviceName = "KukaKr16";
+    std::string deviceName = "KukaKr16";
+    // path to WC, must be with respect to /home, ~/ is not valid
+    const std::string wcFile = "/home/lukas/workcells/Kr16WallWorkCell/Scene.wc.xml";
 
     // final start and end positions
-    rw::math::Q start_pos(6,-3.142,-0.827,-3.002,-3.143,0.099,-1.573);
-    rw::math::Q end_pos(6,1.571,0.006,0.030,0.153,0.762,4.490);
+    rw::math::Q from(6,-3.142,-0.827,-3.002,-3.143,0.099,-1.573);
+    rw::math::Q to(6,1.571,0.006,0.030,0.153,0.762,4.490);
+//    rw::math::Q from(6,-0.2,-0.6,1.5,0.0,0.6,1.2);
+//    //Q to(6,1.7,0.6,-0.8,0.3,0.7,-0.5); // Very difficult for planner
+//    rw::math::Q to(6,1.4,-1.3,1.5,0.3,1.3,1.6);
 
     // test trejectory
-    rw::trajectory::QPath path_test;
-    path_test.emplace_back(start_pos);
-    path_test.emplace_back(end_pos);
+//    rw::trajectory::QPath path_test;
+//    path_test.emplace_back(start_pos);
+//    path_test.emplace_back(end_pos);
+
+    rw::models::WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(wcFile);
+    rw::models::Device::Ptr device = wc->findDevice(deviceName);
+    if (device == NULL) {
+        std::cerr << "Device: " << deviceName << " not found!" << std::endl;
+        return 0;
+    }
+    const rw::kinematics::State state = wc->getDefaultState();
+
+    rw::proximity::CollisionDetector detector(wc, rwlibs::proximitystrategies::ProximityStrategyFactory::makeDefaultCollisionStrategy());
+    rw::pathplanning::PlannerConstraint constraint = rw::pathplanning::PlannerConstraint::make(&detector,device,state);
+
+    /** Most easy way: uses default parameters based on given device
+        sampler: QSampler::makeUniform(device)
+        metric: PlannerUtil::normalizingInfinityMetric(device->getBounds())
+        extend: 0.05 */
+    rw::pathplanning::QToQPlanner::Ptr planner = rwlibs::pathplanners::RRTPlanner::makeQToQPlanner(constraint, device, rwlibs::pathplanners::RRTPlanner::RRTConnect);
+
+
+    std::cout << "Planning from " << from << " to " << to << std::endl;
+    rw::trajectory::QPath path;
+    rw::common::Timer t;
+    t.resetAndResume();
+    planner->query(from,to,path,MAXTIME);
+    t.pause();
 
 
     // change output stuff
@@ -57,7 +93,7 @@ int main()
     std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
     std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
 
-    outputLuaPath(path_test, deviceName);
+    outputLuaPath(path, deviceName);
 
     std::cout.rdbuf(coutbuf); //reset to standard output again
 
