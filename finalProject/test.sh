32710e502b3bda9513cd29e24a16e9c7c7213b13
niive12 <niive12@student.sdu.dk>
2015-11-22 11:47:08 +0100

    added guide to test

--- finalProject/test.sh
+++ finalProject/test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-dir=$1
-app=vision_part-build/VisionPart
+if [ $# -eq 1 ]
+then
+	dir=$1
+	app=vision_part-build/VisionPart
 
-echo -n $(for img in $dir/*png; do $app $img; done | grep "center" | wc -l)
-echo -n " / " 
-echo $(ls -l $dir/*png | wc -l)
+	echo -n $(for img in $dir/*png; do $app $img; done | grep "center" | wc -l)
+	echo -n " / " 
+	echo $(ls -l $dir/*png | wc -l)
+else
+	echo "Test if the marker is detected on a set of images."
+	echo "Input is a folder containing images"
+	echo "Output is the number of successful detections / number of images"
+	echo "The output is verified by printing a message with the word center"
+fi
