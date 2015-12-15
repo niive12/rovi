f1f5edbf58ae1a14ee11835220a9d3ff6ab6ad9c
niive12 <niive12@student.sdu.dk>
2015-12-15 22:08:00 +0100

    merge

--- finalProject/SamplePluginPA10/src/find_limits_dt.cpp
+++ finalProject/SamplePluginPA10/src/find_limits_dt.cpp
@@ -21,41 +21,42 @@ void SamplePlugin::find_limits(){
     // loop through all markers
     for(int marker = 0; marker < _comboBox_rovi_marker->count(); marker++){
         // set the marker
-        _comboBox_rovi_marker->setCurrentIndex(marker);
-        if(_comboBox_rovi_marker->currentText().lastIndexOf("Ideal") == (-1)){
-            //rw::common::Log::log().error() << _comboBox_rovi_marker->currentText().toStdString() << "\n";
+        if( marker != 2 ){ //skip marker 2b
+            _comboBox_rovi_marker->setCurrentIndex(marker);
+            if(_comboBox_rovi_marker->currentText().lastIndexOf("Ideal") == (-1)){
+                //rw::common::Log::log().error() << _comboBox_rovi_marker->currentText().toStdString() << "\n";
 
-            rovi_load_markerImage();
-            // loop through all backgrounds
-            for(int bg = 0; bg < _comboBox_rovi_background->count(); bg++){
-                // set bg
-                _comboBox_rovi_background->setCurrentIndex(bg);
-                rovi_load_bgImage();
-                // start the line:
-                dt_file << _comboBox_rovi_background->currentText().toStdString();
-                avgprocesstime << _comboBox_rovi_background->currentText().toStdString();
-                // loop through the marker movements
-                for(int markerMovement = 0; markerMovement < _comboBox_settings_loadMarker->count(); markerMovement++){
-                    // load markermovement
-                    _comboBox_settings_loadMarker->setCurrentIndex(markerMovement);
-                    loadMarkerMovement();
-                    // find the timestep that still detects the marker
-                    double dt = 0.0;
-                    do{
-                        dt += 0.05;
-                        _spinBox_timestep->setValue(dt);
-                        rovi_processImage();
-                    } while(dt < 1 && _rovi_markerNotTracked);
-                    dt_file << " & " << dt;
-                    avgprocesstime << " & " << _rovi_avgTrackingTime;
+                rovi_load_markerImage();
+                // loop through all backgrounds
+                for(int bg = 0; bg < _comboBox_rovi_background->count(); bg++){
+                    // set bg
+                    _comboBox_rovi_background->setCurrentIndex(bg);
+                    rovi_load_bgImage();
+                    // start the line:
+                    dt_file << _comboBox_rovi_background->currentText().toStdString();
+                    avgprocesstime << _comboBox_rovi_background->currentText().toStdString();
+                    // loop through the marker movements
+                    for(int markerMovement = 0; markerMovement < _comboBox_settings_loadMarker->count(); markerMovement++){
+                        // load markermovement
+                        _comboBox_settings_loadMarker->setCurrentIndex(markerMovement);
+                        loadMarkerMovement();
+                        // find the timestep that still detects the marker
+                        double dt = 0.0;
+                        do{
+                            dt += 0.05;
+                            _spinBox_timestep->setValue(dt);
+                            rovi_processImage();
+                        } while(dt < 1 && _rovi_markerNotTracked);
+                        dt_file << " & " << dt;
+                        avgprocesstime << " & " << _rovi_avgTrackingTime;
+                    }
+                    dt_file << "\\\\ \\hline \n";
+                    avgprocesstime << "\\\\ \\hline \n";
                 }
-                dt_file << "\\\\ \\hline \n";
-                avgprocesstime << "\\\\ \\hline \n";
+                dt_file << "\n\n";
+                avgprocesstime << "\n\n";
             }
-
         }
-        dt_file << "\n\n";
-        avgprocesstime << "\n\n";
     }
     dt_file.close();
     avgprocesstime.close();
